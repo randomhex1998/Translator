4e302702b27b70bf9c179601db15f503c7edba3e
randomhex1998 <randomhex1998@gmail.com>
2023-07-22 12:51:33 +0330

    set bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Main from './Components/Main'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Main/>
+    </>
   )
 }
 
---
+++ src/Components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='h-[100vh] bg-[#eee] flex justify-center items-center'>
+        555
+    </div>
+  )
+}
+
+export default Main
